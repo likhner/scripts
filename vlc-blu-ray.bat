b4c9f0f816caeec39ada81790adab6ca42a01dfd
Arthur Likhner <60031799+likhner@users.noreply.github.com>
2020-12-26 05:23:44 +0200

    Fix link

--- vlc-blu-ray.bat
+++ vlc-blu-ray.bat
@@ -2,7 +2,7 @@
 @echo off
 ver |>NUL find /v "5." && if "%~1"=="" cscript.exe //nologo //e:jscript "%~f0"& exit /b
 mkdir C:\ProgramData\aacs
-powershell $WebClient = New-Object System.Net.WebClient ; $WebClient.DownloadFile('https://gist.githubusercontent.com/likhner/822ccba36c9c88df3975c357c9310642/raw/KEYDB.cfg','C:\ProgramData\aacs\KEYDB.cfg') ; $WebClient.DownloadFile('https://vlc-bluray.whoknowsmy.name/files/mac/libaacs.dylib','C:\Program Files\VideoLAN\VLC\libaacs.dylib')
+powershell $WebClient = New-Object System.Net.WebClient ; $WebClient.DownloadFile('https://gist.githubusercontent.com/likhner/822ccba36c9c88df3975c357c9310642/raw/KEYDB.cfg','C:\ProgramData\aacs\KEYDB.cfg') ; $WebClient.DownloadFile('https://vlc-bluray.whoknowsmy.name/files/win64/libaacs.dll','C:\Program Files\VideoLAN\VLC\libaacs.dll')
 echo Done!
 pause
 exit /B
